847f826d2f4a920d45d696a6542edae5a979db8d
MasqueradeDoggy <sean.d.reavis@gmail.com>
2024-01-10 14:54:18 -0500

    Basic Setup

---
+++ js.js
@@ -0,0 +1,8 @@
+const QNA = [
+    ["What is Considered the 'lucky number'?", "7"],
+    ["Who is the biggest character in the Disney Entertainment franchise?", "Mickey Mouse"],
+    ["Who is Batman?", "Bruce Wayne"],
+    ["What is Superman's weakness?", "Kryptonite"],
+    ["Did Kirito take the MC Masterclass?", "Yes"]
+];
+
