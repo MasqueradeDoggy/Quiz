6ae4973ebfa5510f9e4731acda87ca5ffe86e543
MasqueradeDoggy <sean.d.reavis@gmail.com>
2024-01-12 20:57:43 -0500

    Added More

--- js.js
+++ js.js
@@ -1,3 +1,4 @@
+const questionText = document.querySelector(".qtext")
 const QNA = [
     ["What is Considered the 'lucky number'?", "7"],
     ["Who is the biggest character in the Disney Entertainment franchise?", "Mickey Mouse"],
@@ -5,7 +6,6 @@ const QNA = [
     ["What is Superman's weakness?", "Kryptonite"],
     ["Did Kirito take the MC Masterclass?", "Yes"]
 ];
-const questionText = document.querySelector(".qtext")
 
 function pickArray(){
     const randomArray = Math.floor(Math.random()*QNA.length);
@@ -13,35 +13,46 @@ function pickArray(){
 }
 
 function getInstance(){
-    const instance = pickArray();
-    
-    return instance;
+    return pickArray();
 }
 
-const Question = getInstance();
-const Answer = Question[1].toString();
-const input = document.getElementById('answer');
-const form = document.getElementById('checker');
+let points = 0;
 
 function startGame(){
-    
+    let Question = getInstance();
+    let Answer = Question[1].toString();
+    const input = document.getElementById('answer');
+    const form = document.getElementById('checker');
+    const score = document.getElementById('score');     
 
-    console.log(Question)
+    console.log(Question);
     questionText.textContent = Question[0];
     
+    score.textContent = `Score: ${points}`
+
     form.addEventListener('submit', function(event) {
         event.preventDefault();
         const inptv = input.value.trim()
         if (inptv === Answer) {
                 alert('correct')
+
+                points++
+                score.textContent = `Score: ${points}`
+                console.log(points)
+
+                Question = getInstance();
+                Answer = Question[1].toString();
+                questionText.textContent = Question[0]
             } else {
                 alert('incorrect')
                 console.log(Answer)
 
                 console.log('Input Value:', inptv);
                 console.log('Answer:', Answer.toString().toLowerCase());
-
             }
+        if (points === 5){
+            location.reload()
+        }
     });
 }
 startGame()
